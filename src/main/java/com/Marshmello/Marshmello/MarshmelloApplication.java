3461b794fff3a493c7449c53ec3b88462a42ecb9
kaidey <kdy071115@naver.com>
2024-06-21 21:22:26 +0900

    first commit

---
+++ src/main/java/com/Marshmello/Marshmello/MarshmelloApplication.java
@@ -0,0 +1,13 @@
+package com.Marshmello.Marshmello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshmelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarshmelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Marshmello/Marshmello/MarshmelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Marshmello.Marshmello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarshmelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
